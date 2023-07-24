26bd5f8492dd9e8b0e2c70dc922b27a66d1d3ccd
ZionMyers <24041638-ZionMyers@users.noreply.replit.com>
2023-07-24 17:34:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# ARITHMETIC OPERATORS & PRINT METHOD --> OUTPUT TO CONSOLE
+
+
+print(1+200)# ADDITION
+print(45-67)# SUBTRACTION
+print(45* 3)# MULTIPLICATION
+print(1/23)# DIVIDE
+print(200%20)# MODULUS
+
+
+
+# VARIBLES WITH NUMBER DATA TYPES
+ALVIN = 14
+e= 12
+
+#1ST PRINT 
+print(e)
+
+# VARIBLES WITH LETTER DATA TYPER (string)
+ALVIN = "alvin"
+e= "enasi"
+
+#2nd print
+print(ALVIN)
+
+#concatenation
+print (ALVIN+ " " +e)
